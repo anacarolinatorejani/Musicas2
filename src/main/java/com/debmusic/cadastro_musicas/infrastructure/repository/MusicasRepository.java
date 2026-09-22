1b9ad7a9f48a1b90f161453d4c60b9963d2393e3
ra2457030 <ana.torejani@unesp.br>
2026-09-22 09:32:21 -0300

    first commit

--- src/main/java/com/debmusic/cadastro_musicas/infrastructure/repository/MusicasRepository.java
+++ src/main/java/com/debmusic/cadastro_musicas/infrastructure/repository/MusicasRepository.java
@@ -3,8 +3,7 @@ package com.debmusic.cadastro_musicas.infrastructure.repository;
 import com.debmusic.cadastro_musicas.infrastructure.entitys.Musicas;
 import org.hibernate.internal.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
 public interface MusicasRepository extends JpaRepository <Musicas, Integer> {
-    Optional<Musicas> findByTitulo {String nome}
+    Optional<Musicas> findByNome (String nome);
 }
